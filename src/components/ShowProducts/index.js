3d1323a101b008fed19f2cc7172d80e739f35b83
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2022-08-15 19:21:30 -0300

    Adicionando tabela de produtos

--- src/components/ShowProducts/index.js
+++ src/components/ShowProducts/index.js
@@ -1,16 +1,46 @@
-import * as C from './styles'
+import './styleTable.css'
 
 export const ShowProducts = () => {
     return (
-        <C.Container>
-            <C.CardProducts>
-                <C.TitleProduct>Mouse Gamer</C.TitleProduct>
-                <C.Description>Mouse rgb</C.Description>
-                <C.Quantity>Quantidade: 4</C.Quantity>
-                <C.ButtonDelete>Deletar</C.ButtonDelete>
-                <C.EditButton>Editar</C.EditButton>
-                <C.Price>R$ 40</C.Price>
-            </C.CardProducts>
-        </C.Container>
+        <div className='container'>
+                 <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Nome</th>
+      <th scope="col">Marca</th>
+      <th scope="col">Quantidade</th>
+      <th scope="col">Preço</th>
+      <th scope="col">Preço</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+      <td>@fat</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+      <td>@twitter</td>
+      <td>@twitter</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+        </div>
     )
 }
--- src/components/ShowProducts/styles.js
+++ src/components/ShowProducts/styles.js
@@ -16,24 +16,56 @@ export const Container = styled.div`
 
 export const CardProducts = styled.div`
    
-    min-width: 280px;
-    min-height: 180px;
+    min-width: 250px;
     border-radius: 8px;
-    box-shadow: 0px 2px 5px black;
+    max-height: auto;
+    box-shadow: 0px 0px 3px black;
     background-color: white;
     position: relative;
     right: 60px;
     bottom: 38px;
+    padding: 5px;
+    
+
 `;
 
-export const TitleProduct = styled.h3``;
+export const TitleProduct = styled.h3`
+    font-size: 20px;
+    position: relative;
+    bottom: 15px;
+`;
 
-export const Description = styled.p``;
+export const Description = styled.p`
+    font-size: 18px;
+    position: relative;
+    bottom: 20px;
+`;
 
-export const Quantity = styled.p``;
+export const Quantity = styled.p`
+    display: flex;
+    justify-content: end;
+    position: relative;
+    bottom: 30px;
+`;
 
-export const Price = styled.p``;
+export const Price = styled.p`
+     display: flex;
+    justify-content: end;
+    position: relative;
+    bottom: 45px;
+    font-size: 20px;
+`;
 
-export const ButtonDelete = styled.button``;
+export const ButtonDelete = styled.button`
+    font-size: 20px;
+    border: none;
+    background-color: transparent;
+    color:  #412972;
+`;
 
-export const EditButton = styled.button``;
+export const EditButton = styled.button`
+    font-size: 20px;
+    border: none;
+    background-color: transparent;
+    color:  #412972;
+`;
