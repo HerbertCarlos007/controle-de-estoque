b318e6e2aea958b9808b62a7e7fbdb57592a25f0
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2022-11-18 16:33:51 -0300

    Adicionando sidebar

--- src/components/HomePageStore/index.js
+++ src/components/HomePageStore/index.js
@@ -1,5 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import { Link, useParams, useLocation } from 'react-router-dom'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
+
 import * as C from './styles'
 
 import api from '../../services/api'
@@ -24,16 +33,38 @@ export const HomePageStore = () => {
 
     return (
         <C.Container>
-            <C.Header>
-                <C.HeaderSideLeft>aaa</C.HeaderSideLeft>
-
-                <C.HeaderSideRight>dd</C.HeaderSideRight>
-            </C.Header>
-
-
+            <Navbar key={false} bg="light" expand={false} className="mb-3">
+                <Container fluid>
+                    <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${false}`} />
+                    <Navbar.Brand href="#">Bryan Store</Navbar.Brand>
+                    <Navbar.Offcanvas
+                        id={`offcanvasNavbar-expand-${false}`}
+                        aria-labelledby={`offcanvasNavbarLabel-expand-${false}`}
+                        placement="start"
+                    >
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${false}`}>
+                                Herbert Store
+                            </Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body>
+                            <Nav className="justify-content-end flex-grow-1 pe-3">
+                                <Nav.Link href="#action1">Minha Loja</Nav.Link>
+                                <Nav.Link href="#action2">Estoque</Nav.Link>
+                                <NavDropdown
+                                    title="Perfil"
+                                    id={`offcanvasNavbarDropdown-expand-${false}`}
+                                >
+                                    <NavDropdown.Item href="#action3">Configurações</NavDropdown.Item>
+                                </NavDropdown>
+                            </Nav>
+                        </Offcanvas.Body>
+                    </Navbar.Offcanvas>
+                </Container>
+            </Navbar>
             <C.ContainerProducts>
                 {allProducts && allProducts.map((product) =>
-                    <Link to={`/product/${product.id}`} state={{id: product.id}}>
+                    <Link to={`/product/${product.id}`} state={{ id: product.id }}>
                         <C.CardsProducts>
                             <C.TopContainerCard><C.ImageProduct src={product.imageUrl} /></C.TopContainerCard>
 
