1daad3999a4290eed20a0270650c5a3f263efe69
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2022-12-19 11:50:13 -0300

    Aumentando e diminuindo quantidade de produto pelo id

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -10,18 +10,19 @@ export const Cart = () => {
 
     const [cart, setCart] = useState([])
     const [quantity, setQuantity] = useState(1)
+    const [productIncremented, setProductIncremented] = useState([])
 
     useEffect(() => {
         productsCart()
     }, [])
 
-    
+
     function getCurrency(value) {
         return Number(value).toLocaleString('pt-BR', {
             minimumFractionDigits: 2,
             maximumFractionDigits: 2
-          })
-      };
+        })
+    };
 
     const productsCart = async () => {
         try {
@@ -31,17 +32,40 @@ export const Cart = () => {
         }
     }
 
-    const handleDecrement = () => {
-        if (quantity > 1) {
-            setQuantity(prevCount => prevCount - 1)
+    const handleDecrement = (productId) => {
+       const cartProductsDecremented = cart.map(productsCart => {
 
+        if(productsCart.id === productId && productsCart.quantity > 1) {
+            productsCart.quantity = productsCart.quantity - 1
+        }
+
+        return productsCart
+       })
+
+       setProductIncremented(cartProductsDecremented)
+    }
+
+    const handleIncrement = (productId) => {
+        const cartProductsIncremented = cart.map(productCart => {
+            
+            if(productCart.id === productId) {
+                productCart.quantity = productCart.quantity + 1
+            }
+
+            return productCart
+        })
+
+        setProductIncremented(cartProductsIncremented)
+        console.log(productIncremented)
+    }
+
+    const handleQuantityChange = async (productId, userId, quantity) => {
+        try {
+            await api.patch(`${process.env.REACT_APP_BACKEND_URL}/cartProducts/increment/${userId}/${productId}`,{quantity})
+            await productsCart()
+        } catch (error) {
         }
     }
-
-    const handleIncrement = () => {
-        setQuantity(prevCount => prevCount + 1)
-    }
-
 
 
     return (
@@ -62,15 +86,15 @@ export const Cart = () => {
                         <C.ImageProduct src={item.imageUrl} />
                         <C.ContentTableText>{item.name}</C.ContentTableText>
                         <C.ContentTableText>{getCurrency(item.saleValue)}</C.ContentTableText>
-                        <C.ContentTableText>{quantity}</C.ContentTableText>
+                        <C.ContentTableText>{item.quantity}</C.ContentTableText>
                         <C.ContentTableText>
 
                             <C.ContainerActions>
-                                <C.ButtonLess onClick={handleDecrement}>-</C.ButtonLess>
-                                <C.ButtonAddMore onClick={handleIncrement}>+</C.ButtonAddMore>
+                                <C.ButtonLess onClick={() => handleDecrement(item.id)}>-</C.ButtonLess>
+                                <C.ButtonAddMore onClick={() => handleIncrement(item.id)}>+</C.ButtonAddMore>
                             </C.ContainerActions>
                         </C.ContentTableText>
-                        <C.ContentTableText>{getCurrency(item.saleValue * quantity)}</C.ContentTableText>
+                        <C.ContentTableText>{getCurrency(item.saleValue * item.quantity)}</C.ContentTableText>
 
 
                     </C.ContentTableCart>
