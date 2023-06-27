21f19c8cd0658f5d45fffca9100d093e48be54f2
HerbertCarlos007 <herbert.saopaulino@hotmail.com>
2023-06-26 23:27:38 -0300

    Função de alterar a senha

--- src/components/NewPassword/index.js
+++ src/components/NewPassword/index.js
@@ -1,24 +1,32 @@
 import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom'
 import * as C from './styles'
 import api from '../../services/api'
 
 export const NewPassword = () => {
 
+    const navigate = useNavigate()
+
     const [email, setEmail] = useState('')
 
-    const SendEmailRecoverPassword = async () => {
-        await api.post(`${process.env.REACT_APP_BACKEND_URL}/recover-password`, {
-            email
-        })
+    const SendEmailRecoverPassword = async (e) => {
+        try {
+            e.preventDefault()
+            await api.post(`${process.env.REACT_APP_BACKEND_URL}/recover-password`, {
+                email
+            })
+        } catch (error) {
+
+        }
     }
 
     return (
         <C.Container>
             <C.RecoverPasswordContainer>
                 <C.Title>Recuperar sua senha</C.Title>
-                <C.InputRecoverPassword 
-                placeholder="Digite seu email" 
-                onChange={(e) => setEmail(e.target.value)}
+                <C.InputRecoverPassword
+                    placeholder="Digite seu email"
+                    onChange={(e) => setEmail(e.target.value)}
                 />
                 <C.ButtonSendEmail onClick={SendEmailRecoverPassword}>Enviar email</C.ButtonSendEmail>
             </C.RecoverPasswordContainer>
--- src/components/RecoverPassword/index.js
+++ src/components/RecoverPassword/index.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router";
 import * as C from './styles'
+import api from "../../services/api";
+import { useNavigate } from 'react-router-dom'
 
 export const RecoverPassword = () => {
+    
+    const navigate = useNavigate()
+
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const { id } = useParams()
+
+    const newPassword = async () => {
+
+        try {
+            await api.put(`${process.env.REACT_APP_BACKEND_URL}/forgot-password/${id}`, {
+                password, confirmPassword
+            })
+            navigate('/login')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
-        <>
-        aaaa
-        </>
+        <C.Container>
+            <C.NewPasswordContainer>
+                <C.Title>Digite sua nova senha</C.Title>
+                <C.InputNewPassword 
+                placeholder="Digite sua nova senha"
+                onChange={(e) => setPassword(e.target.value)}
+                />
+                <C.InputNewPassword 
+                placeholder="Confirme sua senha"
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+                <C.ButtonSaveNewPassword onClick={newPassword}>Salvar senha</C.ButtonSaveNewPassword>
+            </C.NewPasswordContainer>
+        </C.Container>
     )
 }
